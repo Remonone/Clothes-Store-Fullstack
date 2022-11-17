7d5525b5fd1e6f0077a3371f56f2c979101c1346
Alia Nguyen <trangkiny@gmail.com>
2022-11-17 13:27:41 +0200

    fullstack template - mern stack

---
+++ api/src/server.ts
@@ -0,0 +1,39 @@
+import errorHandler from 'errorhandler'
+import mongoose from 'mongoose'
+
+import app from './app'
+import { MONGODB_URI } from './util/secrets'
+import logger from './util/logger'
+
+const mongoUrl = MONGODB_URI
+
+/**
+ * Error Handler. Provides error handing middleware
+   only use in development
+ */
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler())
+}
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+
+    // Start Express server
+    app.listen(app.get('port'), () => {
+      console.log(
+        '  App is running at http://localhost:%d in %s mode',
+        app.get('port'),
+        app.get('env')
+      )
+      console.log('  Press CTRL-C to stop\n')
+    })
+
+  })
+  .catch((err: Error) => {
+    console.log(
+      'MongoDB connection error. Please make sure MongoDB is running. ' + err
+    )
+    process.exit(1)
+  })
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function App() {
+  const [grade, setGrade] = useState<number>(67)
+  return (
+    <div>React Application</div>
+  );
+}
+
+export default App;
